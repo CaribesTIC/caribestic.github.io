087509ea77291343512a857b75cfacf723a3b044
ejimenez123 <caribestic123@gmail.com>
2022-05-13 15:42:05 -0400

    deploy

---
+++ assets/vtl_documentacion_intro.md.3cd52533.lean.js
@@ -0,0 +1,3 @@
+import{_ as t,c as a,o as r,b as e,d as s}from"./app.b5e51e70.js";const f='{"title":"\xBFQu\xE9 es Vue Test Library?","description":"","frontmatter":{},"headers":[],"relativePath":"vtl/documentacion/intro.md"}',n={},o=e("h1",{id:"\xBFque-es-vue-test-library",tabindex:"-1"},[s("\xBFQu\xE9 es Vue Test Library? "),e("a",{class:"header-anchor",href:"#\xBFque-es-vue-test-library","aria-hidden":"true"},"#")],-1),i=e("div",{class:"language-"},[e("pre",null,[e("code",null,`npm i -D @testing-library/vue@next
+npm i -D @testing-library/jest-dom
+`)])],-1),c=[o,i];function d(l,u,_,p,h,m){return r(),a("div",null,c)}var v=t(n,[["render",d]]);export{f as __pageData,v as default};
