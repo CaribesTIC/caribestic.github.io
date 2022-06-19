b66f3f6b6a4dcf30d2e280026fd0c92dbda80aa9
ejimenez123 <caribestic123@gmail.com>
2022-06-19 14:47:26 -0400

    deploy

---
+++ assets/vtl_documentacion_guias-de-principios.md.8d3bb11d.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as s,a as n}from"./app.af2b6828.js";const b='{"title":"Gu\xEDas de Principios","description":"","frontmatter":{},"headers":[],"relativePath":"vtl/documentacion/guias-de-principios.md"}',o={},i=n("",6),r=[i];function t(l,d,c,u,p,m){return s(),a("div",null,r)}var f=e(o,[["render",t]]);export{b as __pageData,f as default};
